f2ea1dea1465642cf682baaa3144c3cfc172c2e4
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-04-30 21:17:40 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const heading = "E-commerse website";
+  const description = "i want to make a website about some pizza and coffee";
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 data-ns-test="project-name">{heading}</h1>
+      <p data-ns-test="project-description">{description}</p>
+    </div>
+  );
+};
 
 export default App;
